37245314d7cdc1dbd30e7b325ae887c78eb2b978
unknown <anthony.james.phipps@gmail.com>
2018-03-14 20:52:50 -0400

    Migrated references to Mitre ATT&CK to the wiki

--- Functions/Get-THR_Processes.psm1
+++ Functions/Get-THR_Processes.psm1
@@ -38,10 +38,7 @@ function Get-THR_Processes {
         
     .LINK
        https://github.com/TonyPhipps/THRecon
-       https://attack.mitre.org/wiki/Technique/T1034
-       https://attack.mitre.org/wiki/Technique/T1053
-       https://attack.mitre.org/wiki/Technique/T1134
-       https://attack.mitre.org/wiki/Technique/T1138
+       https://github.com/TonyPhipps/THRecon/wiki/Processes
     #>
 
     param(
