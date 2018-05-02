aa2d95ea3217037381b51f0eab71b4449b4b5f61
unknown <anthony.james.phipps@gmail.com>
2018-05-01 21:25:27 -0400

    Provided copy-item wrapper to avoid errors from smaller file xfer

--- Utilities/PSexec-THR.ps1
+++ Utilities/PSexec-THR.ps1
@@ -1,38 +1,74 @@
-Param(
-    #Local Parameters
-    [string]$ModulePath = "C:\Users\$env:UserName\Documents\WindowsPowerShell\Modules\THRecon\",
-    [string]$Output = "C:\Temp\Results",
-    [string]$ModuleName = "THRecon.psm1",
-    [string]$PSExec = "C:\Program Files\Sysinternals",
-    
-    #Remote Parameters
-    [string]$Computer = "127.0.0.1",
-    [string]$RemoteModulePath = "c$\Windows\Toolkit",
-    [string]$RemoteOutputPath = "c:\Windows\Toolkit\Results",
-    [string]$Command = "Invoke-THR"
-)
+Function Copy-WithProgress { # https://blogs.technet.microsoft.com/heyscriptingguy/2015/12/20/build-a-better-copy-item-cmdlet-2/
+[CmdletBinding()]
 
-if($Command -like "*output*"){
-    Write-Error -Message "Specify remote output via -RemoteOutputPath parameter."
-    exit
+    Param(
+        $Source,
+        $Destination
+    )
+
+    $Source = $Source.tolower()
+    $Filelist = Get-Childitem $Source –Recurse
+    $Total = $Filelist.count
+    $Position = 0
+
+    New-Item -ItemType Directory -Path $Destination -Force
+
+    foreach ($File in $Filelist) {
+        
+        $Filename = $File.Fullname.tolower().replace($Source,"")
+        $DestinationFile = ($Destination + $Filename)
+        
+        Copy-Item $File.FullName -Destination $DestinationFile -Recurse -Force
+        
+        $Position++
+        Write-Progress -Activity "Copying data from $Source to $Destination" -Status "Copying File $Filename" -PercentComplete (($Position/$total)*100)
+    }
+
+    Write-Progress -Activity "Copying data from $Source to $Destination" -Completed
 }
 
-$Command = $Command + " -Output $RemoteOutputPath"
+Function Invoke-psexec_THR
+    {
 
-# Prepare NTFS/Share path versions
-$ModuleShare = $RemoteModulePath.Replace(':', '$')
-$ModuleNTFS = $RemoteModulePath.Replace('$', ':')
-$OutputShare = $RemoteOutputPath.Replace(':', '$')
+    Param(
+        #Local Parameters
+        [string]$ModulePath = "C:\Users\$env:UserName\Documents\WindowsPowerShell\Modules\THRecon\",
+        [string]$Output = "C:\Temp\Results\",
+        [string]$ModuleName = "THRecon.psm1",
+        [string]$PSExec = "C:\Program Files\Sysinternals",
+        
+        #Remote Parameters
+        [string]$Computer = "127.0.0.1",
+        [string]$RemoteModulePath = "c:\Windows\Toolkit\",
+        [string]$RemoteOutputPath = "c:\Windows\Toolkit\Results\",
+        [string]$Command = "Invoke-THR"
+    )
 
-# Stage files
-Copy-Item $ModulePath -Recurse -Force -Destination \\$Computer\$ModuleShare
+    if($Command -like "*output*"){
+        Write-Error -Message "Specify remote output via -RemoteOutputPath parameter."
+        exit
+    }
 
-# Import modulea and execute command
-& $PSExec\PsExec.exe \\$Computer -accepteula powershell -ExecutionPolicy ByPass -nologo -command "& {import-module $ModuleNTFS\$ModuleName; & $Command}"
+    $Command = $Command + " -Output $RemoteOutputPath"
 
-# Retrieve Results
-Copy-Item \\$Computer\$OutputShare -Recurse -Force -Destination $Output
+    # Prepare NTFS/Share path versions
+    $ModuleShare = $RemoteModulePath.Replace(':', '$')
+    $ModuleNTFS = $RemoteModulePath.Replace('$', ':')
+    $OutputShare = $RemoteOutputPath.Replace(':', '$')
 
-# Cleanup
-Remove-Item \\$Computer\$OutputShare -Recurse -Force
-Remove-Item \\$Computer\$ModuleShare -Recurse -Force
+    # Stage files
+    #Copy-Item $ModulePath -Recurse -Force -Destination \\$Computer\$ModuleShare
+    Copy-WithProgress -Source "$ModulePath" -Destination "\\$Computer\$ModuleShare"
+
+    # Import modulea and execute command
+    & $PSExec\PsExec.exe \\$Computer -accepteula powershell -ExecutionPolicy ByPass -nologo -command "& {import-module $ModuleNTFS\$ModuleName; & $Command}"
+
+    # Retrieve Results
+    Copy-WithProgress -Source \\$Computer\$OutputShare -Destination $Output
+
+    # Cleanup
+    Remove-Item \\$Computer\$OutputShare -Recurse -Force
+    Remove-Item \\$Computer\$ModuleShare -Recurse -Force
+}
+
+Invoke-psexec_THR
