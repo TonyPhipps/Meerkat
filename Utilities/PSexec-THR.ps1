af84ac7f99d69d960c422f27d3ab634ec7ce6b71
unknown <anthony.james.phipps@gmail.com>
2018-05-02 20:31:11 -0400

    Merged

--- Utilities/PSexec-THR.ps1
+++ Utilities/PSexec-THR.ps1
@@ -1,4 +1,4 @@
-Param(
+param(
     #Local Parameters
     [string]$ModulePath = "C:\Users\$env:UserName\Documents\WindowsPowerShell\Modules\THRecon\",
     [string]$Output = "C:\Temp\Results\",
@@ -12,7 +12,33 @@ Param(
     [string]$Command = "Invoke-THR"
 )
 
-.\Copy-WithProgress.ps1
+function Copy-WithProgress { # https://blogs.technet.microsoft.com/heyscriptingguy/2015/12/20/build-a-better-copy-item-cmdlet-2/
+
+    param(
+        $Source,
+        $Destination
+    )
+
+    $Source = $Source.tolower()
+    $Filelist = Get-Childitem $Source -Recurse
+    $Total = $Filelist.count
+    $Position = 0
+
+    New-Item -ItemType Directory -Path $Destination -Force
+
+    foreach ($File in $Filelist) {
+        
+        $Filename = $File.Fullname.tolower().replace($Source,"")
+        $DestinationFile = ($Destination + $Filename)
+        
+        Copy-Item $File.FullName -Destination $DestinationFile -Recurse -Force
+        
+        $Position++
+        Write-Progress -Activity "Copying data from $Source to $Destination" -Status "Copying File $Filename" -PercentComplete (($Position/$total)*100)
+    }
+
+    Write-Progress -Activity "Copying data from $Source to $Destination" -Completed
+}
 
 if($Command -like "*output*"){
     Write-Error -Message "Specify remote output via -RemoteOutputPath parameter."
