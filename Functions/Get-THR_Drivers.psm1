dd669f934fd88ae56b5692ce55118065a105fe9b
Tony Phipps <anthony.james.phipps@gmail.com>
2019-03-26 23:08:29 -0400

    updates

--- Functions/Get-THR_Drivers.psm1
+++ Functions/Get-THR_Drivers.psm1
@@ -6,9 +6,6 @@ function Get-THR_Drivers {
     .DESCRIPTION 
         Gets a list of drivers for the given computer(s).
 
-    .PARAMETER Computer  
-        Computer can be a single hostname, FQDN, or IP address.
-
     .EXAMPLE 
         Get-THR_Drivers 
         Get-THR_Drivers SomeHostName.domain.com
@@ -17,13 +14,13 @@ function Get-THR_Drivers {
         Get-ADComputer -filter * | Select -ExpandProperty Name | Get-THR_Drivers
 
     .NOTES
-        Updated: 2018-08-05
+        Updated: 2019-03-26
 
         Contributing Authors:
             Jeremy Arnold
             Anthony Phipps
             
-        LEGAL: Copyright (C) 2018
+        LEGAL: Copyright (C) 2019
         This program is free software: you can redistribute it and/or modify
         it under the terms of the GNU General Public License as published by
         the Free Software Foundation, either version 3 of the License, or
@@ -51,29 +48,11 @@ function Get-THR_Drivers {
 
         $stopwatch = New-Object System.Diagnostics.Stopwatch
         $stopwatch.Start()
-
-        # class Driver
-        # {
-        #     [string] $Computer
-        #     [string] $DateScanned
-            
-        #     [string] $Provider
-        #     [string] $Driver
-        #     [String] $Version
-        #     [datetime] $Date
-        #     [String] $Class
-        #     [string] $DriverSigned
-        #     [string] $OrginalFileName
-        # }
-
-        # $Command = { 
-            
-        # }
     }
 
     process{
 
-        Get-WindowsDriver -Online -ErrorAction SilentlyContinue
+        $ResultsArray = Get-WindowsDriver -Online -ErrorAction SilentlyContinue
 
         foreach ($Result in $ResultsArray) {
             $Result | Add-Member -MemberType NoteProperty -Name "Host" -Value $env:COMPUTERNAME
@@ -82,54 +61,6 @@ function Get-THR_Drivers {
 
         return $ResultsArray | Select-Object Host, DateScanned, ProviderName, Driver, Version, Date, ClassDescription, DriverSignature, OriginalFileName
             
-        # $Computer = $Computer.Replace('"', '')  # get rid of quotes, if present
-        
-        # Write-Verbose ("{0}: Querying remote system" -f $Computer)
-
-        # if ($Computer -eq $env:COMPUTERNAME){
-            
-        #     $ResultsArray = & $Command 
-        # } 
-        # else {
-
-        #     $ResultsArray = Invoke-Command -ComputerName $Computer -ErrorAction SilentlyContinue -ScriptBlock $Command
-        # }
-       
-        # if ($ResultsArray) { 
-          
-        #     $OutputArray = foreach ($driver in $ResultsArray) {
-             
-        #         $output = $null
-        #         $output = [Driver]::new()
-                
-        #         $output.DateScanned = Get-Date -Format o
-        #         $output.Computer = $Computer
-        #         $output.Provider = $driver.ProviderName
-        #         $output.Driver = $driver.Driver
-        #         $output.Version = $driver.Version
-        #         $output.date = $driver.Date
-        #         $output.Class = $driver.ClassDescription
-        #         $output.DriverSigned = $driver.DriverSignature
-        #         $output.OrginalFileName = $driver.OriginalFileName
-
-        #         $output
-        #     }
-
-        #     $total++
-        #     Return $OutputArray
-        
-        # }
-        # else {
-                
-        #     $output = $null
-        #     $output = [Driver]::new()
-
-        #     $output.Computer = $Computer
-        #     $output.DateScanned = Get-Date -Format o
-            
-        #     $total++
-        #     return $output
-        # }
     }
 
     end{
