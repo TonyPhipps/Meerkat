6bf9d8085a69cb62c2fee840e58bb318897089e2
Tony Phipps <anthony.james.phipps@gmail.com>
2019-04-03 21:58:22 -0400

    Updates

--- Functions/Get-THR_NetAdapters.psm1
+++ Functions/Get-THR_NetAdapters.psm1
@@ -1,29 +1,30 @@
 function Get-THR_NetAdapters {
     <#
     .SYNOPSIS 
-        Gets the Interface(s) settings for the given computer(s).
+        Gets network interface settings.
 
     .DESCRIPTION 
-        Gets the Interface(s) settings for the given computer(s) and returns a PS Object.
-
-    .PARAMETER Computer  
-        Computer can be a single hostname, FQDN, or IP address.
+        Gets network interface settings.
 
     .EXAMPLE 
-        Get-THR_NetAdapters 
-        Get-THR_NetAdapters SomeHostName.domain.com
-        Get-Content C:\hosts.csv | Get-THR_NetAdapters
-        Get-THR_NetAdapters -Computer $env:computername
-        Get-ADComputer -filter * | Select -ExpandProperty Name | Get-THR_NetAdapters
+        Get-THR_NetAdapters
+
+    .EXAMPLE 
+        $Targets = Get-ADComputer -filter * | Select -ExpandProperty Name
+        ForEach ($Target in $Targets) {
+            Invoke-Command -ComputerName $Target -ScriptBlock ${Function:Get-THR_NetAdapters} | 
+            Select-Object -Property * -ExcludeProperty PSComputerName,RunspaceID | 
+            Export-Csv -NoTypeInformation "c:\temp\$Target_NetAdapters.csv"
+        }
 
     .NOTES 
-        Updated: 2018-08-05
+        Updated: 2019-04-03
 
         Contributing Authors:
             Jeremy Arnold
             Anthony Phipps
             
-        LEGAL: Copyright (C) 2018
+        LEGAL: Copyright (C) 2019
         This program is free software: you can redistribute it and/or modify
         it under the terms of the GNU General Public License as published by
         the Free Software Foundation, either version 3 of the License, or
@@ -42,8 +43,6 @@ function Get-THR_NetAdapters {
     #>
 
     param(
-    	[Parameter(ValueFromPipeline=$True, ValueFromPipelineByPropertyName=$True)]
-        $Computer = $env:COMPUTERNAME
     )
 
 	begin{
@@ -53,111 +52,36 @@ function Get-THR_NetAdapters {
 
         $stopwatch = New-Object System.Diagnostics.Stopwatch
         $stopwatch.Start()
-        $total = 0
-
-        class NetAdapter
-        {
-            [String] $Computer
-            [string] $DateScanned
-            
-            [String] $FQDN
-            [String] $Description
-            [String] $NetConnectionID
-            [String] $NetConnected
-            [String] $InterfaceIndex
-            [String] $Speed
-            [String] $MACAddress
-            [String] $IPAddress
-            [String] $Subnet
-            [String] $Gateway
-            [String] $DNS
-            [String] $MTU
-            [bool] $PromiscuousMode
-        }
-        
-        $Command = {
-
-            $AdaptersArray = Get-NetAdapter -ErrorAction SilentlyContinue
-            $AdapterConfigArray = Get-CimInstance Win32_NetworkAdapterConfiguration -ErrorAction SilentlyContinue  #get the configuration for the current adapter
-
-            $AdaptersArray = $AdaptersArray | Where-Object {$_.MediaConnectionState -eq "Connected"}
-            
-            foreach ($Adapter in $AdaptersArray) {
-                
-                $AdapterConfig = $AdapterConfigArray | Where-Object {$_.InterfaceIndex -eq $Adapter.IfIndex}
-
-                $Adapter | Add-Member -MemberType NoteProperty -Name ipaddress -Value $AdapterConfig.ipaddress[0]
-                $Adapter | Add-Member -MemberType NoteProperty -Name IPsubnet -Value $AdapterConfig.IPsubnet[0]
-                $Adapter | Add-Member -MemberType NoteProperty -Name DefaultIPGateway -Value $AdapterConfig.DefaultIPGateway
-                $Adapter | Add-Member -MemberType NoteProperty -Name DNSServerSearchOrder -Value $AdapterConfig.DNSServerSearchOrder                
-            }
-
-            return $AdaptersArray
-        } 
 	}
 
     process{
             
-        $Computer = $Computer.Replace('"', '')  # get rid of quotes, if present get-netadapter
+        $AdaptersArray = Get-NetAdapter -ErrorAction SilentlyContinue
+        $AdapterConfigArray = Get-CimInstance Win32_NetworkAdapterConfiguration -ErrorAction SilentlyContinue  #get the configuration for the current adapter
 
-        Write-Verbose ("{0}: Querying remote system" -f $Computer)
-
-        if ($Computer -eq $env:COMPUTERNAME){
-            
-            $ResultsArray = & $Command 
-        } 
-        else {
-
-            $ResultsArray = Invoke-Command -ComputerName $Computer -ErrorAction SilentlyContinue -ScriptBlock $Command
-        }
+        $ResultsArray = $AdaptersArray | Where-Object {$_.MediaConnectionState -eq "Connected"}
         
-        if ($ResultsArray) {
-
+        foreach ($Result in $ResultsArray) {
             
-            $OutputArray = foreach ($Adapter in $ResultsArray) {#loop through the Interfaces and build the outputArray
-                
-                $output = $null
-                $output = [NetAdapter]::new()
+            $AdapterConfig = $AdapterConfigArray | Where-Object {$_.InterfaceIndex -eq $Result.IfIndex}
 
-                $output.Computer = $Computer
-                $output.DateScanned = Get-Date -Format o
-                $output.FQDN = $Adapter.SystemName
-                $output.DESCRIPTION = $Adapter.InterfaceDescription
-                $output.NetConnectionID = $Adapter.Name
-                $output.NetConnected= $Adapter.MediaConnectionState
-                $output.InterfaceIndex = $Adapter.ifIndex
-                $output.Speed = $Adapter.Speed
-                $output.MACAddress = $Adapter.MACAddress
-                $output.IPAddress = $Adapter.ipaddress
-                $output.Subnet = $Adapter.IPsubnet
-                $output.Gateway = $Adapter.DefaultIPGateway
-                $output.DNS = $Adapter.DNSServerSearchOrder
-                $output.MTU = $Adapter.MtuSize
-                $output.PromiscuousMode = $Adapter.PromiscuousMode
-
-                $output
-            }
-
-            $total++
-            Return $OutputArray
+            $Result | Add-Member -MemberType NoteProperty -Name "Host" -Value $env:COMPUTERNAME
+            $Result | Add-Member -MemberType NoteProperty -Name "DateScanned" -Value $DateScanned
+            $Result | Add-Member -MemberType NoteProperty -Name IPAddress -Value ($AdapterConfig.IPAddress -join ", ")
+            $Result | Add-Member -MemberType NoteProperty -Name IPsubnet -Value ($AdapterConfig.IPsubnet -join ", ")
+            $Result | Add-Member -MemberType NoteProperty -Name DefaultIPGateway -Value $AdapterConfig.DefaultIPGateway
+            $Result | Add-Member -MemberType NoteProperty -Name DNSServerSearchOrder -Value $AdapterConfig.DNSServerSearchOrder                
         }
-        else {
-                
-            $output = $null
-            $output = [NetAdapter]::new()
 
-            $output.Computer = $Computer
-            $output.DateScanned = Get-Date -Format o
-            
-            $total++
-            return $output
-        }
+        return $AdaptersArray | Select-Object Host, DateScanned, SystemName, InterfaceDescription, Name, MediaConnectionState, 
+        ifIndex, Speed, MacAddress, IPAddress, IPSubnet, DefaultIPGateway, DNSServerSearchOrder, MtuSize, PromiscuousMode
     }
 
     end{
 
         $elapsed = $stopwatch.Elapsed
 
-        Write-Verbose ("Total Systems: {0} `t Total time elapsed: {1}" -f $total, $elapsed)
+        Write-Verbose ("Total time elapsed: {0}" -f $elapsed)
+        Write-Verbose ("Ended at {0}" -f (Get-Date -Format u))
     }
 }
