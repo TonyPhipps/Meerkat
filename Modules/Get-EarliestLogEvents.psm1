6f76b49692d0cd23403f6473af291c79330f7749
jgsmith97 <jackgsmith28@gmail.com>
2022-11-23 16:01:33 -0500

    Add Earliest Event Log Module

---
+++ Modules/Get-EarliestLogEvents.psm1
@@ -0,0 +1,95 @@
+function Get-EarliestLogEvents {
+    <#
+    .SYNOPSIS
+        Gets earliest log event.
+
+    .DESCRIPTION
+        Gets earliest log event. Security, Application, and System logs are queried. 
+
+    .EXAMPLE 
+        Get-EventsEarliestLogEvents
+
+    .EXAMPLE 
+        Invoke-Command -ComputerName remoteHost -ScriptBlock ${Function:Get-EarliestLogEvents} | 
+        Select-Object -Property * -ExcludeProperty PSComputerName,RunspaceID | 
+        Export-Csv -NoTypeInformation ("c:\temp\EventLogs.csv")
+
+    .EXAMPLE 
+        $Targets = Get-ADComputer -filter * | Select -ExpandProperty Name
+        ForEach ($Target in $Targets) {
+            Invoke-Command -ComputerName $Target -ScriptBlock ${Function:Get-EarliestLogEvents} | 
+            Select-Object -Property * -ExcludeProperty PSComputerName,RunspaceID | 
+            Export-Csv -NoTypeInformation ("c:\temp\" + $Target + "_EventLogs.csv")
+        }
+
+    .NOTES
+        Updated: 2022-11-23
+
+        Contributing Authors:
+            Anthony Phipps, Jack Smith
+            
+        LEGAL: Copyright (C) 2019
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+    
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+        
+    .LINK
+       https://github.com/TonyPhipps/Meerkat
+       https://github.com/TonyPhipps/Meerkat/wiki/EventLogs
+    #>
+
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [datetime] $StartTime,
+
+        [Parameter()]
+        [datetime] $EndTime
+    )
+
+    begin{
+
+        $DateScanned = Get-Date -Format u
+        Write-Information -InformationAction Continue -MessageData ("Started Get-EarliestLogEvents at {0}" -f $DateScanned)
+
+        $stopwatch = New-Object System.Diagnostics.Stopwatch
+        $stopwatch.Start()
+
+    }
+
+    process{
+
+        $Logs = Get-WinEvent -ListLog Security,Application,System | Where-Object { ($_.RecordCount -gt 0) }
+
+        $ResultsArray = Foreach ($Log in $Logs){
+    
+            Get-WinEvent -LogName $Log.LogName -MaxEvents 1 -Oldest -ErrorAction SilentlyContinue
+        }
+    
+        foreach ($Result in $ResultsArray) {
+            $Result | Add-Member -MemberType NoteProperty -Name "Host" -Value $env:COMPUTERNAME
+            $Result | Add-Member -MemberType NoteProperty -Name "DateScanned" -Value $DateScanned
+        }
+    
+        return $ResultsArray | Select-Object Host, DateScanned, LogName, TimeCreated, Id, 
+        Task, Level, Opcode, KeywordsDisplayNames, UserId, Message
+     
+    }
+
+    end{
+
+        $elapsed = $stopwatch.Elapsed
+
+        Write-Verbose ("Total time elapsed: {0}" -f $elapsed)
+        Write-Verbose ("Ended at {0}" -f (Get-Date -Format u))
+    }
+}                                                              
