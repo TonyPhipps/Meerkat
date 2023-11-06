f1729f6208b06fd1723e2b1b1c57cfc3009bf911
Tony Phipps <anthony.james.phipps@gmail.com>
2023-11-06 16:47:04 -0700

    added kdsrootkey check and creation

--- Utilities/Schedule-Meerkat.ps1
+++ Utilities/Schedule-Meerkat.ps1
@@ -6,6 +6,42 @@ $Server = "ServerName"
 $ScriptName = "C:\Meerkat-Task.ps1"
 $AtTime = "1/30/2023 2:00:00 AM"
 
+# Create the MSA
+
+try {
+    Test-ADServiceAccount -Identity $MSAName
+    $MSAExists = $true
+}
+catch {
+    $MSAExists = $false
+}
+
+if(-not $MSAExists) {
+    Add-WindowsFeature RSAT-AD-PowerShell
+    Import-Module ActiveDirectory
+    
+    $KDSKeys = Get-KdsRootKey
+
+    if ($null -ne $KDSKeys -and $KDSKeys.Count -gt 0 ) {
+        Write-Information -InformationAction Continue -MessageData ("'{0}' KDS Root Key(s) exists already, skipping KdsRootKey creation." -f $KDSKeyCount)
+    }
+    elseif ($null -eq $KDSKeys) {
+        Write-Information -InformationAction Continue -MessageData ("Permissions not available. Skipping KdsRootKey creation." -f $KDSKeyCount)
+    }
+    else {
+        Write-Information -InformationAction Continue -MessageData ("No KDS Root Key found. Creating KdsRootKey")
+        Add-KdsRootKey -EffectiveTime ((Get-Date).AddHours(-10))
+    }
+
+
+# Create the Scheduled Task
+    $Identity = Get-ADComputer -identity $Server
+    New-ADServiceAccount -Name $MSAName -Enabled $true -RestrictToSingleComputer -KerberosEncryptionType AES256
+    Add-ADComputerServiceAccount -Identity $Identity -ServiceAccount $MSAName
+
+    Install-ADServiceAccount -Identity ($MSAName + "$")
+} else { Write-Information -InformationAction Continue -MessageData ("MSA '{0}' exists already, skipping creation." -f $MSAName) }
+
 $Action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy Bypass -Windowstyle Hidden -File `"$ScriptName`""
 $Trigger = New-ScheduledTaskTrigger -Once -At $AtTime -RepetitionDuration (New-TimeSpan -Days (365 * 20)) -RepetitionInterval (New-TimeSpan -Minutes 60)
 $Principal = New-ScheduledTaskPrincipal -UserId ($MSAName + "$") -RunLevel Highest -LogonType Password
