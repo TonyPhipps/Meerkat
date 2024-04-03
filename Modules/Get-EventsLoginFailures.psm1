7bea235a1e220ef21dffa6a95289a6e8ef71d177
Tony Phipps <anthony.james.phipps@gmail.com>
2024-04-03 13:47:14 -0600

    Better error handling

--- Modules/Get-EventsLoginFailures.psm1
+++ Modules/Get-EventsLoginFailures.psm1
@@ -29,12 +29,12 @@ function Get-EventsLoginFailures {
         }
 
     .NOTES
-        Updated: 2023-08-18
+        Updated: 2024-04-03
 
         Contributing Authors:
             Anthony Phipps, Jack Smith
             
-        LEGAL: Copyright (C) 2023
+        LEGAL: Copyright (C) 2024
         This program is free software: you can redistribute it and/or modify
         it under the terms of the GNU General Public License as published by
         the Free Software Foundation, either version 3 of the License, or
@@ -81,7 +81,7 @@ function Get-EventsLoginFailures {
 
     process{
         
-        $ResultsArray = Get-WinEvent -FilterHashtable @{ LogName="Security"; ID=4625; StartTime=$StartTime; EndTime=$EndTime } -ErrorAction SilentlyContinue
+        $ResultsArray = Get-WinEvent -FilterHashtable @{ LogName="Security"; ID=4625; StartTime=$StartTime; EndTime=$EndTime } -ErrorAction Ignore
         
             foreach ($Result in $ResultsArray) {
                 $Result | Add-Member -MemberType NoteProperty -Name "UserSID" -Value ($Result.Properties[4].Value)
